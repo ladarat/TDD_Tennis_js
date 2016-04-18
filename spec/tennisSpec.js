81c0c2cbe30ffc0d181a554f60d78d0ec3453689
ladarat <ladarat.y@gmail.com>
2016-04-18 10:16:41 +0700

    tdd tennis

--- spec/tennisSpec.js
+++ spec/tennisSpec.js
@@ -1,13 +1,233 @@
 describe("Tennis", function() {
-    it("should show LOVE ALL when start game", function() {
+    it("should show LOVE LOVE when start game", function() {
         var tennis = new Tennis();
         var result = tennis.score();
         expect(result).toEqual("LOVE LOVE");
     });
 
-    xit("should show FIFTEEN LOVE when start game", function() {
-        var tennis = new Tennis();   
+    it("should show FIFTEEN LOVE when player A win first point", function() {
+        var tennis = new Tennis();
+        tennis.playerAWin();    
         var result = tennis.score();
         expect(result).toEqual("FIFTEEN LOVE");
+    });
+
+    it("should show LOVE FIFTEEN when player B win", function() {
+        var tennis = new Tennis();
+        tennis.playerBWin();
+        var result = tennis.score();
+        expect(result).toEqual("LOVE FIFTEEN");
+    });
+
+    it("should show FIFTEEN FIFTEEN when player A win one point and player B win one point", function() {
+        var tennis = new Tennis();
+        tennis.playerAWin();
+        tennis.playerBWin();        
+        var result = tennis.score();
+        expect(result).toEqual("FIFTEEN FIFTEEN");
+    });
+
+    it("should show THIRTY LOVE when player A win twice", function() {
+        var tennis = new Tennis();
+        tennis.playerAWin();
+        tennis.playerAWin();      
+        var result = tennis.score();
+        expect(result).toEqual("THIRTY LOVE");
+    });
+
+    it("should show LOVE THIRTY when player B win twice", function() {
+        var tennis = new Tennis();
+        tennis.playerBWin();
+        tennis.playerBWin();      
+        var result = tennis.score();
+        expect(result).toEqual("LOVE THIRTY");
+    });
+
+    it("should show FORTY LOVE when player A win 3 times", function() {
+        var tennis = new Tennis();
+        tennis.playerAWin();
+        tennis.playerAWin();
+        tennis.playerAWin();      
+        var result = tennis.score();
+        expect(result).toEqual("FORTY LOVE");
+    });
+
+    it("should show THIRTY FIFTEEN when player A win 2 times and player B win 1 times", function() {
+        var tennis = new Tennis();
+        tennis.playerAWin();
+        tennis.playerAWin();
+        tennis.playerBWin();      
+        var result = tennis.score();
+        expect(result).toEqual("THIRTY FIFTEEN");
+    });
+
+    it("should show FIFTEEN THIRTY when player B win 2 times and player A win 1 times", function() {
+        var tennis = new Tennis();
+        tennis.playerBWin();
+        tennis.playerBWin();
+        tennis.playerAWin();      
+        var result = tennis.score();
+        expect(result).toEqual("FIFTEEN THIRTY");
+    });
+
+    it("should show LOVE FORTY when player B win 2 times and player A win 1 times", function() {
+        var tennis = new Tennis();
+        tennis.playerBWin();
+        tennis.playerBWin();
+        tennis.playerBWin();    
+        var result = tennis.score();
+        expect(result).toEqual("LOVE FORTY");
+    });
+
+    it("should show playerAWin when player A win 4 times", function() {
+        var tennis = new Tennis();
+        tennis.playerAWin();
+        tennis.playerAWin();
+        tennis.playerAWin();  
+        tennis.playerAWin();  
+        var result = tennis.score();
+        expect(result).toEqual("PLAYER A WIN");
+    });
+
+    it("should show FORTY FIFTEEN when player A win 3 times and player B win 1 times", function() {
+        var tennis = new Tennis();
+        tennis.playerAWin();
+        tennis.playerAWin();
+        tennis.playerAWin();  
+        tennis.playerBWin();  
+        var result = tennis.score();
+        expect(result).toEqual("FORTY FIFTEEN");
+    });
+
+    it("should show THIRTY THIRTY when player A win 2 times and player B win 2 times", function() {
+        var tennis = new Tennis();
+        tennis.playerAWin();
+        tennis.playerAWin();
+        tennis.playerBWin();  
+        tennis.playerBWin();  
+        var result = tennis.score();
+        expect(result).toEqual("THIRTY THIRTY");
+    });
+
+    it("should show FIFTEEN FORTY when player A win 1 times and player B win 3 times", function() {
+        var tennis = new Tennis();
+        tennis.playerAWin();
+        tennis.playerBWin();
+        tennis.playerBWin();  
+        tennis.playerBWin();  
+        var result = tennis.score();
+        expect(result).toEqual("FIFTEEN FORTY");
+    });
+
+    it("should show PLAYER B WIN when player B win 4 times", function() {
+        var tennis = new Tennis();
+        tennis.playerBWin();
+        tennis.playerBWin();
+        tennis.playerBWin();  
+        tennis.playerBWin();  
+        var result = tennis.score();
+        expect(result).toEqual("PLAYER B WIN");
+    });
+
+    it("should show FORTY THIRTY when player A win 3 times and player B win 2 times", function() {
+        var tennis = new Tennis();
+        tennis.playerAWin();
+        tennis.playerAWin();
+        tennis.playerAWin();  
+        tennis.playerBWin();  
+        tennis.playerBWin();
+        var result = tennis.score();
+        expect(result).toEqual("FORTY THIRTY");
+    });
+
+    it("should show THIRTY FORTY when player A win 2 times and player B win 3 times", function() {
+        var tennis = new Tennis();
+        tennis.playerAWin();
+        tennis.playerAWin();
+        tennis.playerBWin();  
+        tennis.playerBWin();  
+        tennis.playerBWin();
+        var result = tennis.score();
+        expect(result).toEqual("THIRTY FORTY");
+    });
+
+    it("should show DUECE when player A win 3 times and player B win 3 times", function() {
+        var tennis = new Tennis();
+        tennis.playerAWin();
+        tennis.playerAWin();
+        tennis.playerAWin();
+        tennis.playerBWin();  
+        tennis.playerBWin();  
+        tennis.playerBWin();
+        var result = tennis.score();
+        expect(result).toEqual("DUECE");
+    });
+
+    it("should show PLAYER A AVD when player A win 4 times and player B win 3 times", function() {
+        var tennis = new Tennis();
+        tennis.playerAWin();
+        tennis.playerAWin();
+        tennis.playerAWin();
+        tennis.playerAWin();  
+        tennis.playerBWin();  
+        tennis.playerBWin();
+        tennis.playerBWin();
+        var result = tennis.score();
+        expect(result).toEqual("PLAYER A ADV");
+    });
+
+    it("should show PLAYER B AVD when player A win 3 times and player B win 4 times", function() {
+        var tennis = new Tennis();
+        tennis.playerAWin();
+        tennis.playerAWin();
+        tennis.playerAWin();
+        tennis.playerBWin();  
+        tennis.playerBWin();  
+        tennis.playerBWin();
+        tennis.playerBWin();
+        var result = tennis.score();
+        expect(result).toEqual("PLAYER B ADV");
+    });
+
+    it("should show PLAYER A WIN AFTER AVD when player A win 5 times and player B win 3 times", function() {
+        var tennis = new Tennis();
+        tennis.playerAWin();
+        tennis.playerAWin();
+        tennis.playerAWin();
+        tennis.playerAWin();  
+        tennis.playerAWin();  
+        tennis.playerBWin();
+        tennis.playerBWin();
+        tennis.playerBWin();
+        var result = tennis.score();
+        expect(result).toEqual("PLAYER A WIN");
+    });
+
+    it("should show PLAYER B WIN AFTER AVD when player A win 3 times and player B win 5 times", function() {
+        var tennis = new Tennis();
+        tennis.playerAWin();
+        tennis.playerAWin();
+        tennis.playerAWin();
+        tennis.playerBWin();  
+        tennis.playerBWin();  
+        tennis.playerBWin();
+        tennis.playerBWin();
+        tennis.playerBWin();
+        var result = tennis.score();
+        expect(result).toEqual("PLAYER B WIN");
+    });
+
+    it("should show DUECE AFTER AVD when player A win 4 times and player B win 4 times", function() {
+        var tennis = new Tennis();
+        tennis.playerAWin();
+        tennis.playerAWin();
+        tennis.playerAWin();
+        tennis.playerAWin();  
+        tennis.playerBWin();  
+        tennis.playerBWin();
+        tennis.playerBWin();
+        tennis.playerBWin();
+        var result = tennis.score();
+        expect(result).toEqual("DUECE");
     });
 });
--- src/tennis.js
+++ src/tennis.js
@@ -1,6 +1,49 @@
 function Tennis(){
-    this.score = score;
-    function score(){
-        return "LOVE LOVE";
+   this.scoreA = 0;
+   this.scoreB = 0;
+
+    this.score = function() {
+        var scoreA = "";
+        var scoreB = "";
+        var scoreMap = {
+            0: "LOVE",
+            1: "FIFTEEN",
+            2: "THIRTY",
+            3: "FORTY"
+        };        
+
+        if(this.scoreA === this.scoreB  && this.scoreA >= 3){
+            return "DUECE";
+        }
+
+        if(this.scoreA > 3 || this.scoreB > 3){
+            var scoreDiff = this.scoreA - this.scoreB;
+            if(scoreDiff === 1){
+                return "PLAYER A ADV";
+            }
+            if(scoreDiff === -1){
+                return "PLAYER B ADV";
+            }
+            if(scoreDiff > 1){
+                return "PLAYER A WIN";
+            }
+            if(scoreDiff < -1){
+                return "PLAYER B WIN";
+            }
+        }
+
+        if(this.scoreA < 4 && this.scoreB < 4){
+            scoreA = scoreMap[this.scoreA];
+            scoreB = scoreMap[this.scoreB];
+            return scoreA + " " + scoreB;
+        }
     }
+
+    this.playerAWin = function() {
+        this.scoreA += 1;
+    };
+
+    this.playerBWin = function () {
+        this.scoreB += 1;
+    };
 }
