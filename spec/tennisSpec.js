50444c16cd1b1bba7c547c8548de3a309f2505ce
ladarat <ladarat.y@gmail.com>
2016-04-18 13:25:07 +0700

    use forEach for test case dup All

--- spec/tennisSpec.js
+++ spec/tennisSpec.js
@@ -22,6 +22,126 @@ describe("Tennis", function() {
             playerBScore : 1,
             result : "LOVE FIFTEEN"
         },
+        {
+            testName : "should show FIFTEEN FIFTEEN when player A win one point and player B win one point",
+            playerAScore : 1,
+            playerBScore : 1,
+            result : "FIFTEEN FIFTEEN"
+        },
+        {
+            testName : "should show THIRTY LOVE when player A win twice",
+            playerAScore : 2,
+            playerBScore : 0,
+            result : "THIRTY LOVE"
+        },
+        {
+            testName : "should show LOVE THIRTY when player B win twice",
+            playerAScore : 0,
+            playerBScore : 2,
+            result : "LOVE THIRTY"
+        },
+        {
+            testName : "should show FORTY LOVE when player A win 3 times",
+            playerAScore : 3,
+            playerBScore : 0,
+            result : "FORTY LOVE"
+        },
+        {
+            testName : "should show THIRTY FIFTEEN when player A win 2 times and player B win 1 times",
+            playerAScore : 2,
+            playerBScore : 1,
+            result : "THIRTY FIFTEEN"
+        },
+        {
+            testName : "should show FIFTEEN THIRTY when player B win 2 times and player A win 1 times",
+            playerAScore : 1,
+            playerBScore : 2,
+            result : "FIFTEEN THIRTY"
+        },
+        {
+            testName : "should show LOVE FORTY when player B win 2 times and player A win 1 times",
+            playerAScore : 0,
+            playerBScore : 3,
+            result : "LOVE FORTY"
+        },
+        {
+            testName : "should show playerAWin when player A win 4 times",
+            playerAScore : 4,
+            playerBScore : 0,
+            result : "PLAYER A WIN"
+        },
+        {
+            testName : "should show FORTY FIFTEEN when player A win 3 times and player B win 1 times",
+            playerAScore : 3,
+            playerBScore : 1,
+            result : "FORTY FIFTEEN"
+        },
+        {
+            testName : "should show THIRTY THIRTY when player A win 2 times and player B win 2 times",
+            playerAScore : 2,
+            playerBScore : 2,
+            result : "THIRTY THIRTY"
+        },
+        {
+            testName : "should show FIFTEEN FORTY when player A win 1 times and player B win 3 times",
+            playerAScore : 1,
+            playerBScore : 3,
+            result : "FIFTEEN FORTY"
+        },
+        {
+            testName : "should show PLAYER B WIN when player B win 4 times",
+            playerAScore : 0,
+            playerBScore : 4,
+            result : "PLAYER B WIN"
+        },
+        {
+            testName : "should show FORTY THIRTY when player A win 3 times and player B win 2 times",
+            playerAScore : 3,
+            playerBScore : 2,
+            result : "FORTY THIRTY"
+        },
+        {
+            testName : "should show THIRTY FORTY when player A win 2 times and player B win 3 times",
+            playerAScore : 2,
+            playerBScore : 3,
+            result : "THIRTY FORTY"
+        },
+        {
+            testName : "should show DUECE when player A win 3 times and player B win 3 times",
+            playerAScore : 3,
+            playerBScore : 3,
+            result : "DUECE"
+        },
+        {
+            testName : "should show PLAYER A AVD when player A win 4 times and player B win 3 times",
+            playerAScore : 4,
+            playerBScore : 3,
+            result : "PLAYER A ADV"
+        },
+        {
+            testName : "should show PLAYER B AVD when player A win 3 times and player B win 4 times",
+            playerAScore : 3,
+            playerBScore : 4,
+            result : "PLAYER B ADV"
+        },
+        {
+            testName : "should show PLAYER A WIN AFTER AVD when player A win 5 times and player B win 3 times",
+            playerAScore : 5,
+            playerBScore : 3,
+            result : "PLAYER A WIN"
+        },
+        {
+            testName : "should show PLAYER B WIN AFTER AVD when player A win 3 times and player B win 5 times",
+            playerAScore : 3,
+            playerBScore : 5,
+            result : "PLAYER B WIN"
+        },
+        {
+            testName : "should show DUECE AFTER AVD when player A win 4 times and player B win 4 times",
+            playerAScore : 4,
+            playerBScore : 4,
+            result : "DUECE"
+        }
 
     ].forEach(function(element){
         it(element.testName, function() {
@@ -29,121 +149,6 @@ describe("Tennis", function() {
             playerBGetPoint(element.playerBScore);
             expect(tennis.score()).toEqual(element.result);
         });
-    });
-
-    it("should show FIFTEEN FIFTEEN when player A win one point and player B win one point", function() {
-        playerAGetPoint(1)
-        playerBGetPoint(1);
-        expect(tennis.score()).toEqual("FIFTEEN FIFTEEN");
-    });
-
-    it("should show THIRTY LOVE when player A win twice", function() {
-        playerAGetPoint(2);
-        playerBGetPoint(0);
-        expect(tennis.score()).toEqual("THIRTY LOVE");
-    });
-
-    it("should show LOVE THIRTY when player B win twice", function() {
-        playerBGetPoint(2);
-        expect(tennis.score()).toEqual("LOVE THIRTY");
-    });
-
-    it("should show FORTY LOVE when player A win 3 times", function() {
-        playerAGetPoint(3);
-        expect(tennis.score()).toEqual("FORTY LOVE");
-    });
-
-    it("should show THIRTY FIFTEEN when player A win 2 times and player B win 1 times", function() {
-        playerAGetPoint(2);
-        playerBGetPoint(1);
-        expect(tennis.score()).toEqual("THIRTY FIFTEEN");
-    });
-
-    it("should show FIFTEEN THIRTY when player B win 2 times and player A win 1 times", function() {
-        playerBGetPoint(2);
-        playerAGetPoint(1);
-        expect(tennis.score()).toEqual("FIFTEEN THIRTY");
-    });
-
-    it("should show LOVE FORTY when player B win 2 times and player A win 1 times", function() {
-        playerBGetPoint(3);
-        expect(tennis.score()).toEqual("LOVE FORTY");
-    });
-
-    it("should show playerAWin when player A win 4 times", function() {
-        playerAGetPoint(4);
-        expect(tennis.score()).toEqual("PLAYER A WIN");
-    });
-
-    it("should show FORTY FIFTEEN when player A win 3 times and player B win 1 times", function() {
-        playerAGetPoint(3); 
-        playerBGetPoint(1);
-        expect(tennis.score()).toEqual("FORTY FIFTEEN");
-    });
-
-    it("should show THIRTY THIRTY when player A win 2 times and player B win 2 times", function() {
-        playerAGetPoint(2);
-        playerBGetPoint(2);
-        expect(tennis.score()).toEqual("THIRTY THIRTY");
-    });
-
-    it("should show FIFTEEN FORTY when player A win 1 times and player B win 3 times", function() {
-        playerAGetPoint(1);
-        playerBGetPoint(3);
-        expect(tennis.score()).toEqual("FIFTEEN FORTY");
-    });
-
-    it("should show PLAYER B WIN when player B win 4 times", function() {
-        playerBGetPoint(4);
-        expect(tennis.score()).toEqual("PLAYER B WIN");
-    });
-
-    it("should show FORTY THIRTY when player A win 3 times and player B win 2 times", function() {
-        playerAGetPoint(3);
-        playerBGetPoint(2);
-        expect(tennis.score()).toEqual("FORTY THIRTY");
-    });
-
-    it("should show THIRTY FORTY when player A win 2 times and player B win 3 times", function() {
-        playerAGetPoint(2);
-        playerBGetPoint(3);
-        expect(tennis.score()).toEqual("THIRTY FORTY");
-    });
-
-    it("should show DUECE when player A win 3 times and player B win 3 times", function() {
-        playerAGetPoint(3);
-        playerBGetPoint(3);
-        expect(tennis.score()).toEqual("DUECE");
-    });
-
-    it("should show PLAYER A AVD when player A win 4 times and player B win 3 times", function() {
-        playerAGetPoint(4);
-        playerBGetPoint(3);
-        expect(tennis.score()).toEqual("PLAYER A ADV");
-    });
-
-    it("should show PLAYER B AVD when player A win 3 times and player B win 4 times", function() {
-        playerAGetPoint(3);
-        playerBGetPoint(4)
-        expect(tennis.score()).toEqual("PLAYER B ADV");
-    });
-
-    it("should show PLAYER A WIN AFTER AVD when player A win 5 times and player B win 3 times", function() {
-        playerAGetPoint(5);
-        playerBGetPoint(3);
-        expect(tennis.score()).toEqual("PLAYER A WIN");
-    });
-
-    it("should show PLAYER B WIN AFTER AVD when player A win 3 times and player B win 5 times", function() {
-        playerAGetPoint(3);
-        playerBGetPoint(5);
-        expect(tennis.score()).toEqual("PLAYER B WIN");
-    });
-
-    it("should show DUECE AFTER AVD when player A win 4 times and player B win 4 times", function() {
-        playerAGetPoint(4);
-        playerBGetPoint(4);
-        expect(tennis.score()).toEqual("DUECE");
     });
 
     it("should show THIRTY FIFTEEN when A win 1 ponit and B win 1 point then A win 1 point again", function() {
